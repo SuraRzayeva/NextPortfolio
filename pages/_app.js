c6ee318a484d9b00345fb748c58741a2be6f8b08
Sura Rzayeva <sura.rzayeva@gmail.com>
2021-01-15 19:45:16 +0100

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import ContextWrapper from '../context/ContextWrapper'
 import Menu from '../components/atoms/Menu'
 import Header from '../components/atoms/Header'
@@ -6,9 +7,20 @@ import GlobalStyle from '../style/GlobalStyle'
 import Sidebar from '../components/atoms/Sidebar'
 import Footer from '../components/atoms/Footer'
 import Cursor from '../components/atoms/Cursor'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
-  const [menuActive, setMenuActive] = useState(false)
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   const [overlay, setOverlay] = useState({
     status: false,
     data: '',
