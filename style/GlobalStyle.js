01377d01f320d05df5731c48a256baeadc9789d6
Sura Rzayeva <sura.rzayeva@gmail.com>
2021-01-19 08:00:42 +0100

    Helvetica to Montserrat

--- style/GlobalStyle.js
+++ style/GlobalStyle.js
@@ -21,7 +21,7 @@ body {
     width: 100vw;
     height: 100vh;
     box-sizing: border-box;
-    font-family: 'Helvetica', sans-serif;
+    font-family: 'Montserrat', sans-serif;
     background-color: ${Colors.white};
     overflow-x: hidden;
     overflow-y: ${(props) => (props.overlay ? 'hidden' : 'scroll')};
